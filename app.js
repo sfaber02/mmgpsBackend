f4e18e0a5719003d1d0cfdfd7d9d8141e076ed1b
Shawn Faber <96348445+sfaber02@users.noreply.github.com>
2022-04-25 19:08:16 -0400

    changed access control

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ app.use(cors({
 app.use(express.json());
 
 app.use((req, res, next) => {
-    res.append('Access-Control-Allow-Origin', ['https://mmgps.netlify.app']);
+    console.log ('HELLO');
+    res.append('Access-Control-Allow-Origin', ['*']);
     res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.append('Access-Control-Allow-Headers', 'Content-Type');
     next();
